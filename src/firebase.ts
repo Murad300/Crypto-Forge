70c3c8c51e196efb408a1a333b514fd88c8a6da3
Murad Shaikh <148976296+Murad300@users.noreply.github.com>
2026-06-08 12:10:37 +0600

    Delete src directory

--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "../firebase-applet-config.json";
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const ADMIN_UIDS = ["YKzK471nwRcztvOjpHZp78hA5cz1", "admin@gmail.com", "admin@cryptoforge.online", "admin@smartbd.com"];
